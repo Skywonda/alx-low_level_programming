119c361db39ef544cd12a88feaefa7fd69641025
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-08 00:05:26 +0100

    writing c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+
+/* writing my first c program
+ * am so exicted
+*/ thats all for the trash
+
 int main(void)
 {
-	puts("Programming is like building a multilingualpuzzle /n");
+	puts("Programming is like building a multilingual puzzle \n");
 	return(0);
 }
