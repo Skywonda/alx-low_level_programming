0141c66cec4b574c0a070ba5dff8bc583192a0b0
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-09 20:46:26 +0100

    last digit wahala

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("last digit of %d is %d",n, n%10);
+	printf("last digit of %d is %d ",n, n%10);
 	if (n % 10 > 5)
-		printf("is greater than 5/n");
+		printf("and is greater than 5/n");
 	else if (n % 10 == 0 )
 		printf("is zero");
 	else if (n % 10 < 0 && n % 10 != 0)
