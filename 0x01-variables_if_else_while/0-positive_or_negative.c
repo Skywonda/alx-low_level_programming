c4bb51190756250cca2bed59d05e05144e8ef74a
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-09 11:18:24 +0100

    conditions in c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,14 +20,14 @@ int main(void)
 	else if (n == 0)
 	
 	(
-	printf("%d is zero/n", n);
+		printf("%d is zero/n", n);
 	)
 
 
 	 else if (n > 0)
         
         (
-        printf("%d is positive/n", n);
+        	printf("%d is positive/n", n);
         )
 
 	return (0);
