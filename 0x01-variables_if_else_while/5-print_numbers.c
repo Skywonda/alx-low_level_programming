65453a3ed6d137ecccd22a53b9a7c7d1ef555a41
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-11 00:58:18 +0100

    testing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /** 
- * main - Return all base 10 numbers
+ * main - Return all single base 10 numbers
  * followed by a new line
  * Return: Always return 0 (success)
  */
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int num;
-	for (num =  0; num < 10; num ++)
+
+	for (num =  0; num < 10; num++)
 		printf("%d", num);
 	printf("\n");
 	return (0);
-	
 }
