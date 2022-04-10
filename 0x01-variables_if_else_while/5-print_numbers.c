c6a6dee9e6ef2060db9c641c7cb0e042d035d484
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-11 00:42:44 +0100

    print numbers from base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** 
+ * main - Return all base 10 numbers
+ * followed by a new line
+ * Return: Always return 0 (success)
+
+int main(void)
+{
+	int num;
+	for (num =  0; num < 10; num ++)
+		printf(num);
+	printf('\n');
+	return (0);
+	
+}
