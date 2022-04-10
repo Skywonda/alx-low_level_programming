e853dedee2888e6509516813187adba122e04882
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-10 13:01:29 +0100

    alphabet in both in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function return all alphabets both in upper and lowercase
+ * followed by a new line
+ * Return: Always return 0 (suceess)
+ */
+
+int main(void)
+
+{
+	char a;
+	char b;
+
+	for (a = 'a'; a <= 'z'; a++;)
+		putchar(a);
+	for b = 'A'; a <= 'Z'; a++;
+		putchar(b)
+
+	putchar ('\n')
+	return (0)
+}
