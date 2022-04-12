9d383224dfb791ec3ebe3781f9195579cb7537d8
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-12 01:48:11 +0100

    reversed alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,13 +3,13 @@
 /**
  * main - prints the alphabets in a reverse pattern
  * followed by a new line
- * Return: Always return success (0)
+ * Return: Always return 0 (success)
  */
 
 int main(void)
 {
 	char a;
-	
+
 	for (a = 'z'; a >= 'a'; a--)
 		putchar(a);
 	putchar('\n');
