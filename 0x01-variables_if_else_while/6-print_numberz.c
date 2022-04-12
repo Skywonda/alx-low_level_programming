c4215d0897c6f208eded162de6d8ef5df118bed6
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-12 01:09:04 +0100

    still dealing with numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single number in base 10
+ * followed by a new line
+ * Return: Always return 0 (suceess)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+		putchar("%d", num);
+	putchar("\n");
+	return (0);
+}
