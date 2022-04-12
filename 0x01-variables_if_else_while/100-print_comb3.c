5da2e747b50aec0497667e5852ed98be31433e1a
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-12 22:16:02 +0100

    message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,15 @@ int main(void)
 	int num1 = 48;
 	int num2 = 48;
 
-	while (num1 < 60)
-	{
-		if (num1 != num2 && num1 < num2)
+	while (num1 <58)
+		num1 = 48
+		while (num2 < 58)
 		{
-			putchar(num1);
-			putchar(num2);
-		}
-		num1++;
-		num2++;
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1);
+				putchar(num2);
+			}
 	}
 	return (0);
 }
