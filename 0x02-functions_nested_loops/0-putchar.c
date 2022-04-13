3a39e7ff1ebadbfb39b344d5bfb27eea09f7686f
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-13 22:22:46 +0100

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
+
 /**
- * main - prints putchar
- * followed by a new line
- * Return: Always return 0 (success)
+ * main - Entry point
+ *
+ * Return: nothing, return void
  */
 
 int main(void)
 {
-	char text[8] = "_putchar";
-	int i;
+putchar('');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	return (0);
+return (0);
 }
