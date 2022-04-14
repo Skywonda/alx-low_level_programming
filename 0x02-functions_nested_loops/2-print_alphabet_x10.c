777370213b263ee9b2697ebbaeb9ee57f10cf5e7
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-14 10:21:06 +0100

    main

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+    int counter = 0;
+
+    while (counter < 10)
+    {
+        char a;
+
+        for (a = 'a'; a < 'z'; a++)
+        {
+            _putchar(a);
+        }
+        _putchar('\n');
+        counter++;
+    }
+}
