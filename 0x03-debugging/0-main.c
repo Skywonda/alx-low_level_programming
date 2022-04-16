67d7eb5d117c9d3b0c098117351f6726c5bcffee
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-16 09:55:01 +0100

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints if integer is negative or postive
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	i = 0; 
+
+	positive_or_negative(i)
+	return(0);
+}
