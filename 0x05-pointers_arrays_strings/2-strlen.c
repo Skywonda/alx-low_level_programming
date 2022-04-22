59f20d46449aec096ec33bbcd8a2cc6afb023e05
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-23 00:24:17 +0100

    testing

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return length of a given string
+ * @s: is the string itself
+ * Return: Return string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	length = strlen(*s);
+	return (length);
+}
