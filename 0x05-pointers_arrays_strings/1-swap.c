822328330b335ffdacb6c568352dccedcebe0247
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-23 00:08:08 +0100

    testing

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of the function parameter
+ * @a: first pointer
+ * @b: second pointer
+ * Return: reuturn void
+ */
+void swap_int(int *a, int *b)
+{
+	int *temp = *a;
+	*a = *b;
+	*b = *temp;
+}
