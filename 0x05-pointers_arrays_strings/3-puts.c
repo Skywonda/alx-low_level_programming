0b71a548cfed7a430a1ef519b7aecab274626481
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-25 00:02:35 +0100

    prints string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * _puts - prints a string
- * @str: just a string
- * followed by a new line
- * Return: return void
+ * _puts - prints string
+ * @str: string
+ * Return: void
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (i != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
+
 	_putchar('\n');
 }
