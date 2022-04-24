ae240255efab81ec14cb2d1f087d41470cfe64e3
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-24 22:46:17 +0100

    under testing

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * followed by a new line
+ * Return: return void
+ */
+
+void -puts(char *str)
+{
+	for (i =0; i != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
