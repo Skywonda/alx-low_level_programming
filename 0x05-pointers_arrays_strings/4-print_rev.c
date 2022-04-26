ef035983b4e6eb6c49ec295955a28f016adcd913
skyrealZ <ayobamisamuel85@gmail.com>
2022-04-26 01:18:30 +0100

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+/**
+ * print_rev - prints string in a reversed order
+ * @s: A string
+ * followed by a new line
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	while (i != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
